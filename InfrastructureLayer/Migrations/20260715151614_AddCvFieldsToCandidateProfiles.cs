51c6370173377cf4d20fea6b1370aa47cbeffce3
hienn12454 <hien12454@gmail.com>
2026-06-28 18:42:06 +0700

    SCRUM-145: Add EF Core migration (4/6)

---
+++ InfrastructureLayer/Migrations/20260715151614_AddCvFieldsToCandidateProfiles.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfrastructureLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCvFieldsToCandidateProfiles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CvBlobPath",
+                table: "CandidateProfiles",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CvContentType",
+                table: "CandidateProfiles",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CvFileName",
+                table: "CandidateProfiles",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CvUploadedAt",
+                table: "CandidateProfiles",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CvBlobPath",
+                table: "CandidateProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "CvContentType",
+                table: "CandidateProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "CvFileName",
+                table: "CandidateProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "CvUploadedAt",
+                table: "CandidateProfiles");
+        }
+    }
+}
