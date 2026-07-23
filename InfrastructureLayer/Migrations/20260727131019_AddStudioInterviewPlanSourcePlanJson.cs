62402175eaeafd91cf7f9a17362d3628118684f7
namnm309 <namnm309@gmail.com>
2026-07-23 09:11:03 +0700

    SCRUM-367: Add EF Core migration (13/20)

---
+++ InfrastructureLayer/Migrations/20260727131019_AddStudioInterviewPlanSourcePlanJson.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfrastructureLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudioInterviewPlanSourcePlanJson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SourcePlanJson",
+                table: "studio_interview_plans",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SourcePlanJson",
+                table: "studio_interview_plans");
+        }
+    }
+}
